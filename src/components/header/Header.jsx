8bdb070bbf02a296421ce6e77bb857e920692ac0
Wildanzr <graita.sukma@gmail.com>
2022-01-26 15:52:38 +0700

    #6 - Mobile header and jumbotron

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,35 +7,37 @@ const Header = () => {
     return(
         <section className="h-full w-full border-box transition-all duration-500 linear lg:px-24 md:px-20 px-3 py-3 bg-cyan-300">
             <div className="navbar-1-1">
-            <div className="container mx-auto flex flex-wrap flex-row items-center justify-between">
-                <a href="" className="flex font-medium items-center">
-                    <img src={Logo} className="items-center" width="100" height="80" />
-                </a>
-                <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden block">
-                <svg className="w-6 h-6" fill="none" stroke="#092A33" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-                </label>
-                <input className="hidden" type="checkbox" id="menu-toggle" />
-                <div
-                className="hidden lg:flex lg:items-center lg:w-auto w-full lg:ml-auto lg:mr-auto flex-wrap items-center text-base justify-center"
-                id="menu">
-                <nav
-                    className="lg:space-x-12 space-x-0 lg:flex items-center justify-between text-base pt-8 lg:pt-0 lg:space-y-0 space-y-6 text-[#003366]">
-                    <a href="" className="block nav-link active font-medium">Home</a>
-                    <a href="" className="block nav-link">Event</a>
-                    <a href="" className="block nav-link">Company</a>
-                    <a href="" className="block nav-link">Contacts</a>
-                </nav>
-                </div>
+                <div className="container mx-auto flex flex-wrap flex-row items-center justify-between md:px-0 md:py-1">
+                    <a href="" className="flex font-medium items-center">
+                        <img src={Logo} className="items-center" width="100" height="80" />
+                    </a>
+                    <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden block">
+                    <svg className="w-6 h-6" fill="none" stroke="#092A33" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                    </label>
+                    <input className="hidden" type="checkbox" id="menu-toggle" />
+                    <div
+                    className="hidden lg:flex lg:items-center lg:w-auto w-full lg:ml-auto lg:mr-auto flex-wrap items-center text-base justify-center"
+                    id="menu">
+                    <nav
+                        className="items-center justify-center text-base text-[#003366] pt-4 space-x-0 space-y-4 lg:space-x-20 lg:flex lg:pt-0 lg:space-y-0">
+                        <a href="" className="text-center font-semibold block nav-link hover:font-bold">Home</a>
+                        <a href="" className="text-center font-semibold block nav-link hover:font-bold">Event</a>
+                        <a href="" className="text-center font-semibold block nav-link hover:font-bold">Company</a>
+                        <a href="" className="text-center font-semibold block nav-link hover:font-bold">Contacts</a>
+                    </nav>
+                    </div>
 
-                <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
-                <button
-                    className="bg-[#003366] text-white items-center border-0 py-1 px-6 focus:outline-none rounded-full font-medium text-base mt-6 lg:mt-0">
-                    Login
-                </button>
+                    <div className="hidden text-center lg:flex lg:items-center lg:w-auto w-full" id="menu">
+                        <a href="">
+                            <button
+                                className="bg-[#003366] text-white items-center border-0 py-1 px-6 focus:outline-none rounded-full font-medium text-base mt-6 lg:mt-0">
+                                Login
+                            </button>
+                        </a>
+                    </div>
                 </div>
-            </div>
             </div>            
         </section>
     )
--- src/components/main/Jumbotron.jsx
+++ src/components/main/Jumbotron.jsx
@@ -3,20 +3,20 @@ import Hero from "../../images/hero.png"
 
 const Jumbotron = () => {
     return (
-        <div className="flex items-center justify-center bg-cyan-300 text-justify py-20">
-            <div className="w-full px-6 pt-6 md:px-24 md:pt-0">
-                <h1 className="text-2xl md:text-6xl text-[#003366] font-bold uppercase py-5">Find your <br/> best partner! </h1>
-                <p className="text-base text-[#003366] mb-5">
-                    Mulai dari acara musik, diskusi, workshop hingga online webinar bisa mendapat sponsor dengan mudah. Buat Event Online sekarang lebih mudah di EVENTPLUS.
+        <div className="flex flex-col-reverse py-8 items-center bg-cyan-300 text-center lg:py-20 lg:flex-row lg:text-justify">
+            <div className="w-11/12 mx-auto my-auto text-[#003366] lg:mx-24">
+                <h1 className="text-4xl font-bold uppercase py-3 md:py-5 md:text-6xl">Find your <br/> best partner! </h1>
+                <p className="text-base lg:text-lg lg:mb-5">
+                    Mulai dari acara musik, diskusi, workshop hingga online webinar bisa mendapat sponsor dengan mudah. Buat Event Online sekarang lebih mudah di EventPlus.
                 </p>
                 <button
-                    className="bg-[#003366] text-white items-center border-0 py-1 px-6 focus:outline-none rounded-full text-base mt-6 lg:mt-0">
+                    className="bg-[#003366] text-white items-center py-2 px-8 rounded-full text-base focus:outline-none mt-5 lg:mt-0">
                     Get Started
                 </button>
             </div>
             <div className="w-full">
                 <div className="container">
-                    <img src={Hero} width="80%" className="mx-auto" />
+                    <img src={Hero} className="mx-auto w-4/5" />
                 </div>
             </div>
         </div>
