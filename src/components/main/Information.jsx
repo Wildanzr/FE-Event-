613dd262010169992e7fc7404515dd4bc99353db
Wildanzr <graita.sukma@gmail.com>
2022-02-20 17:41:21 +0700

    #24 - Components information

--- src/components/main/Information.jsx
+++ src/components/main/Information.jsx
@@ -1,47 +1,68 @@
-import React from 'react'
-import AboutUs from "../../images/group1.png"
-import Solution from "../../images/group2.png"
+import React from "react";
+import Info from "../../images/about.png";
 
 const Information = () => {
-    return (
-        <>
-        <div className="w-full flex flex-col justify-center items-center bg-white text-[#003366] py-0 lg:flex-row">
-            <div className="flex w-full justify-center items-center my-5 mx-10 lg:justify-end">
-                <img src={AboutUs} className="w-3/5 h-3/5 lg:h-4/6 lg:w-4/6 items-center justify-center"/>
-            </div>
-            <div className="w-11/12 justify-center items-center my-5 mx-10">
-                <h3 className="text-2xl uppercase font-bold text-center mb-2 lg:text-left">About Us</h3>
-                <p className="text-base text-center mx-auto lg:text-lg lg:text-left">
-                    EventPlus merupakan sosial media yang diperuntukkan untuk mendokumentasikan, mempromosikan, dan sebagai jembatan untuk mempertemukan pihak event organizer dengan perusahaan atau organisasi yang ingin memberikan sponsor. 
-                </p>
-                
-                <a href="" className="flex items-center justify-center lg:justify-start">
-                    <button className="bg-[#4DB6AC] px-3 py-1 rounded-full text-base text-semibold text-white mt-2 lg:mt-5">
-                        About Us
-                    </button>
-                </a>
-            </div>
+  return (
+    <div className="w-full bg-[#F2F5FA] text-[#003366] py-10">
+      <div className="flex flex-col justify-center items-center py-0 lg:flex-row">
+        <div className="flex w-full justify-center items-center my-2 mx-10 lg:justify-end">
+          <img
+            src={Info}
+            className="w-3/5 h-3/5 lg:h-3/5 lg:w-3/5 items-center justify-center"
+          />
         </div>
+        <div className="w-11/12 justify-center items-center my-2 mx-10">
+          <h3 className="text-2xl uppercase font-bold text-center mb-2 lg:text-left">
+            About Us
+          </h3>
+          <p className="text-lg text-center mx-auto lg:text-lg lg:text-left">
+            Event plus merupakan platform yang dapat membantu kegiatan
+            sponsorship dalam pencarian sponsor untuk pendanaan suatu acara
+            maupun kegiatan yang tidak terbatas pada lembaga atau perusahaan
+            tertentu.
+          </p>
 
-        <div className="w-full flex flex-col justify-center items-center bg-white text-[#003366] py-0 lg:flex-row-reverse">
-            <div className="flex w-full justify-center my-5 mx-10 lg:justify-start">
-                <img src={Solution} className="w-3/5 h-3/5 lg:h-4/6 lg:w-4/6 items-center justify-center"/>
-            </div>
-            <div className="w-11/12 justify-center items-center my-5 mx-10">
-                <h3 className="text-2xl uppercase font-bold text-center mb-2 lg:text-right">Our Solution</h3>
-                <p className="text-base text-center mx-auto lg:text-lg lg:text-right">
-                    Dengan menggunakan EventPlus, event organizer dapat mengirimkan proposal sponsorship kepada perusahaan atau organisasi. Begitu juga perusahaan atau organisasi dapat menawarkan pendanaan kegiatan kepada event yang dikehendaki.
-                </p>
-                
-                <a href="" className="flex items-center justify-center lg:justify-end">
-                    <button className="bg-[#4DB6AC] px-3 py-1 rounded-full text-base text-semibold text-white mt-2 lg:mt-5">
-                        Our Solution
-                    </button>
-                </a>
-            </div>
+          <a
+            href=""
+            className="flex items-center justify-center lg:justify-start"
+          >
+            <button className="bg-[#003366] px-5 py-1 rounded-lg text-base font-nunito text-semibold text-white mt-2 lg:mt-5">
+              About Us
+            </button>
+          </a>
         </div>
-        </>
-    )
-}
+      </div>
 
-export default Information
+      <div className="flex flex-col justify-center items-center py-0 lg:flex-row-reverse">
+        <div className="flex w-full justify-center my-2 mx-10 lg:justify-start">
+          <img
+            src={Info}
+            className="w-3/5 h-3/5 lg:h-3/5 lg:w-3/5 items-center justify-center"
+          />
+        </div>
+        <div className="w-11/12 justify-center items-center my-2 mx-10">
+          <h3 className="text-2xl uppercase font-bold text-center mb-2 lg:text-right">
+            Our Solutions
+          </h3>
+          <p className="text-lg text-center mx-auto lg:text-lg lg:text-right">
+            Sistem pada Eventplus akan memberikan kemudahan dan keamanan dalam
+            perjanjian kerja sama sponsorship. Eventplus bertindak sebagai pihak
+            penengah dalam komunikasi antara penyelenggara acara dengan sponsor
+            atau mitra dalam melakukan kerja sama sponsorship.
+          </p>
+
+          <a
+            href=""
+            className="flex items-center justify-center lg:justify-end"
+          >
+            <button className="bg-[#003366] px-5 py-1 rounded-lg text-base font-nunito text-semibold text-white mt-2 lg:mt-5">
+              Our Solutions
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Information;
